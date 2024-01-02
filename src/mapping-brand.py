ace2838acabcf73f9dffcde3eca52f361b5f3394
MARIO <mario@MARIOs-MacBook-Pro.local>
2024-01-02 10:45:01 +0100

    feat: first commit

---
+++ src/mapping-brand.py
@@ -0,0 +1,9 @@
+my_dict = {
+    "blbg": "Billabong",
+    "adids": "Adidas",
+    "zara": "Zara",
+    "ripc": "Rip Curl",
+    "pull": "Pull & Bear",
+    "jack": "Jack & Jones"
+    "gosport": "Go Sport"
+}
